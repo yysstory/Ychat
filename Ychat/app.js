0a6209785be373adb68e3cb58e6c7a063b5988a6
yysstory <yysstory@gmail.com>
2014-10-16 20:29:36 +0900

    ..

--- Ychat/app.js
+++ Ychat/app.js
@@ -9,7 +9,7 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , fs =require('fs')
-  , socketio = require('socket.io');
+  , io = require('socket.io');
 
 var app = express();
 
@@ -22,7 +22,8 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -34,7 +35,16 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  fs.readFile('HTMLPage.html',function(error,data){
+  fs.readFile('/view/index.ejs','utf8',function(error,data){
 	//아무것도 없어도 되능거?
   });
 });
+
+io.sockets.on('connection',function(socket){
+	//message 이벤트
+	socket.on('message',function(){
+		io.sockets.emit('message',data);
+	});
+});
+
+
